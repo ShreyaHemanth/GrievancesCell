749f75bba7c2523c95eb6755fa262a8d70b6ac21
Prajwal B R <prajwal331@gmail.com>
2019-12-02 10:08:19 +0530

    Doing table retrieval

--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createBrowserHistory } from 'history';
-import { Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
 
-import 'assets/scss/material-kit-react.scss?v=1.8.0';
+import "assets/scss/material-kit-react.scss?v=1.8.0";
 
 // pages for this product
-import Components from 'views/Components/Components.js';
-import LandingPage from 'views/LandingPage/LandingPage.js';
-import ProfilePage from 'views/ProfilePage/ProfilePage.js';
-import LoginPage from 'views/LoginPage/LoginPage.js';
-import IndexPage from 'views/IndexPage/IndexPage';
+import Components from "views/Components/Components.js";
+import LandingPage from "views/LandingPage/LandingPage.js";
+import ProfilePage from "views/ProfilePage/ProfilePage.js";
+import LoginPage from "views/LoginPage/LoginPage.js";
+import IndexPage from "views/IndexPage/IndexPage";
 import ComplaintsPage from "views/ComplaintsPage/ComplaintsPage";
 import GrievancesPage from "views/GrievancesPage/GrievancesPage";
+import ViewComplaints from "views/ViewComplaints/ViewComplaints";
+import GrievancesSection from "views/GrievancSection/GrievancesSection";
 var hist = createBrowserHistory();
 
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
-      <Route path='/landing-page' component={LandingPage} />
-      <Route path='/profile-page' component={ProfilePage} />
-      <Route path='/login-page' component={LoginPage} />
-      <Route path='/complaints-page' component={ComplaintsPage} />
-      <Route path='/grievances-page' component={GrievancesPage} />
+      <Route path="/landing-page" component={LandingPage} />
+      <Route path="/profile-page" component={ProfilePage} />
+      <Route path="/login-page" component={LoginPage} />
+      <Route path="/complaints-page" component={ComplaintsPage} />
+      <Route path="/grievances-page" component={GrievancesPage} />
+      <Route path="/grievances-section" component={GrievancesSection} />
+      <Route path="/view-complaints" component={ViewComplaints} />
 
-
-      <Route path='/t' component={Components} />
-      <Route path='/' component={IndexPage} />
+      <Route path="/t" component={Components} />
+      <Route path="/" component={IndexPage} />
     </Switch>
   </Router>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
